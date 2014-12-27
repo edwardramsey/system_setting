a622ed26b379c0b1303dfa94dfa0d6471b32a332
edwardramsey <chenyuqisife@gmail.com>
2014-12-27 14:51:08 +0800

    add echo info to linux_setting.sh

--- linux_setting.sh
+++ linux_setting.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 PKG_MANAGER=$( command -v yum || command -v apt-get || echo "no pkgManager")
+LOG_FILE="config.log"
 
 echo "$PKG_MANAGER"
 if [ 'whoami'="root" ]; then
-	echo "root user"
+	echo "root user" > $LOG_FILE
 else
-	echo "need add sudo"
+	echo "need add sudo" >$LOG_FILE
 	if [ command -v apt-get ]; then
 		PKG_MANAGER = sudo PKG_MANAGER
 	fi
@@ -14,34 +15,47 @@ fi
 $PKG_MANAGER update -y
 $PKG_MANAGER upgrage -y
 
-echo "install vim"
+echo "install vim \n" > $LOG_FILE
 $PKG_MANAGER install vim -y
 
 # use own .vimrc setting
 if [ -f ~/.vimrc ]; then 
-	rm ~/.vimrc
+	echo "exist .vimrc \n" >$LOG_FILE
 else
-	git clone https://github.com/edwardramsey/systrm-setting
-	cd system-setting 
-	mv .vimrc ~/
+	git clone https://github.com/edwardramsey/system-setting/linux_vim/.vimrc 
+	mv ~/system-setting/linux_vim/.vimrc ~/
 	rm -rf system-setting/
+	echo "istall .vimrc" >$LOG_FILE
 fi
 
-if [ ! -d ~/.vim ]; then
-	mkdir -p ~/.vim/
+# use vundle to manage vim plugin
+if [ ! -d ~/.vim/bundle ]; then
+	mkdir -p ~/.vim/bundle
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundl/Vundle.vim
+	echo "already install vundle \n" >$LOG_FILE
 fi
+
 # theme
-#git clone https://github.com/sickill/vim-monokai
+if [ ! -d ~/.vim/colors ]; then
+	mkdir -p ~/.vim/colors
+	git clone https://github.com/sickill/vim-monokai
+	mv vim-monokai/colors/monokai.vim ~/.vim/colors
+	rm -rf vim-monokai
+	echo "install monokai theme \n" >$LOG_FILE
+fi
 
 # install C++ pkg
 $PKG_MANAGER install -y build-essential 
+echo "install C++ pkg \n" > $LOG_FILE
 
 # install python pkg
 $PKG_MANAGER install -y python-pip python-dev
-pip install virtualenv
+sudo pip install virtualenv
+echo "install python pkg \n" >$LOG_FILE
 
 # install ngnix
 $PKG_MANAGER install -y nginx
+echo "have install nginx pkg already \n" >$LOG_FILE
 
 # install mysql
 $PKG_MANAGER install -y mysql-server
