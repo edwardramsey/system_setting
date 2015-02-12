991f65b11fc234892280e0f1c0d94066e3e63187
edwardramsey <824854390@qq.com>
2015-02-12 15:16:47 +0800

    remove test word

--- alias/my_own_alias.sh
+++ alias/my_own_alias.sh
@@ -5,5 +5,3 @@ export CLICOLOR=1
 
 # git setting
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %Cblue<%an>%Creset' --abbrev-commit"
-
-# add for test
